4b1b4f7fbe3fa48f6612b0a27ddb97778ff0cd65
Rick Seeger <rick.seeger@gmail.com>
2017-03-13 21:28:16 -0700

    fix formatting, remove dupes, fix erroneous ids

--- rpc-examples.sh
+++ rpc-examples.sh
@@ -1,15 +1,12 @@
-# account info
-#curl -X POST -d '{ "method" : "account_info", "params" : [ { "account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"} ] }' http://s1.ripple.com:51234
+printf "\n[LAST CLOSED LEDGER]\n"
+curl -X POST -d '{ "method": "ledger_closed", "params": [ {} ] }' http://s1.ripple.com:51234 2>/dev/null | python -m json.tool
 
-# last closed ledger
-#curl -X POST -d '{ "method": "ledger_closed", "params": [ {} ] }' http://s1.ripple.com:51234
+printf "\n[TRANSACTION DETAIL]\n"
+curl -X POST -d '{ "method": "tx", "params": [ { "transaction": "E24ACA2991C4DAA79B198EE9ADF9F8CB0E47561BA9B99CBA3B330E1AEB8D76A8", "binary": false } ] }' http://s1.ripple.com:51234 2>/dev/null | python -m json.tool
 
-# transaction detail
-#curl -X POST -d '{ "method": "tx", "params": [ { "transaction": "E08D6E9754025BA2534A78707605E0601F03ACE063687A0CA1BDDACFCD1698C7", "binary": false } ] }' http://s1.ripple.com:51234
+printf "\n[ACCOUNT INFO]\n"
+curl -X POST -d '{ "method" : "account_info", "params" : [ { "account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"} ] }' http://s1.ripple.com:51234 2>/dev/null | python -m json.tool
 
-# ledger entry
-#curl -X POST -d '{ "id": 1, "method": "ledger_entry", "type": "account_root", "account_root": "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59", "ledger_index": "validated"  }' http://s1.ripple.com:51234
 
-# get balance
-#curl -X POST -d '{ "id": 2, "method": "account_info", "account": "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59" }' http://s1.ripple.com:51234
+
 
